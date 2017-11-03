611429ef3f842214a22248ca46460200166aa86e
DahengZhang <Daheng_Zhang@outlook.com>
2017-11-03 10:42:18 +0800

    通用版本

---
+++ currency-0/src/app.js
@@ -0,0 +1,9 @@
+// 与此文件相关的js也被热更新，不添加词段代码，js更改时不会触发热更新
+if(module.hot) {
+    module.hot.accept();
+}
+
+import appCss from '@/app.css'
+
+let text = 'Hello World添加ssssww'
+document.getElementById('root').innerText = text
