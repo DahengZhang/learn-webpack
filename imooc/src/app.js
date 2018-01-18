78b284f54a8ca391bee2b87f0750612594316022
DahengZhang <Daheng_Zhang@outlook.com>
2018-01-18 21:56:54 +0800

    解决不同

--- imooc/src/app.js
+++ imooc/src/app.js
@@ -1,16 +1,16 @@
 const page = 'a';
 if (page === 'a')
-    import('./common.a').then(comA => {
-        console.log("comA", comA)
-    })
+   import('./common.a').then(comA => {
+      console.log("comA", comA)
+   })
 else
-    import('./common.b').then(comB => {
-        console.log("comB", comB)
-    })
+   import('./common.b').then(comB => {
+      console.log("comB", comB)
+   })
 
 let arr = [1, 2, 3, 4];
 let arrB = arr.map(item => {
-    item * 2;
+   item * 2;
 });
 
 arr.includes(8);
